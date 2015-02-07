fe41b4a02135ebf656c668fe8142e8ff522522e0
Jason Trost <jason.trost@gmail.com>
2015-02-07 06:57:19 -0500

    src_ip -> src and dst_ip -> dst in splunk output

--- splunk.py
+++ splunk.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python
 
 def format(message):
-    return u' '.join(['{}={}'.format(name, value) for name, value in message.items() if value])
+
+    outmsg = dict(message)
+    
+    if 'src_ip' in outmsg:
+        outmsg['src'] = outmsg['src_ip']
+        del outmsg['src_ip']
+
+    if 'dest_ip' in outmsg:
+        outmsg['dest'] = outmsg['dest_ip']
+        del outmsg['dest_ip']
+
+    return u' '.join(['{}={}'.format(name, value) for name, value in outmsg.items() if value])
